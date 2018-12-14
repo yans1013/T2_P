9d5aefb53dec78e0f5df61e5a7fc58779f1d8a3b
yans1013 <95225443@qq.com>
2018-12-14 11:26:26 +0800

    Initial Commit

---
+++ T2_P/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  T2_P
+//
+//  Created by 寺先生 on 2018/12/14.
+//  Copyright © 2018 wu yuansi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

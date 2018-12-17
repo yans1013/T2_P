3a0135121f645e3fc131eca1de9677db5290d602
yans1013 <95225443@qq.com>
2018-12-17 14:46:46 +0800

    bis zum 17-12

--- T2_P/ViewController.swift
+++ T2_P/ViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var InputTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        InputTextField.placeholder = "Hallo Guys!"
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        NotificationCenter.default.post(name: NSNotification.Name.init(rawValue: "keyboardDidShowNotification"), object: "haha")
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: NSNotification.Name.init(rawValue: "keyboardDidShowNotification"), object: nil)
+    }
+    
+    @objc private func keyboardDidShow(notification: Notification){
+        guard  let info = notification.object as? String else {
+            return
+        }
+        debugPrint(info)
     }
 
 
